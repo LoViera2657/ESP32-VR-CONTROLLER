c888606edcbb5e92c572803c3db6022480dead53
LoViera2657 <lvabrandalize@gmail.com>
2026-09-04 10:08:25 -0300

    Update ESP32 - VR CONTROLLER.ino

--- ESP32 - VR CONTROLLER.ino
+++ ESP32 - VR CONTROLLER.ino
@@ -25,19 +25,33 @@ void loop(){
     int16_t ax, ay, az;
     int16_t gx, gy, gz;
 
+    float ax_ms2, ay_ms2, az_ms2;
+    float gx_dps, gy_dps, gz_dps;
+
     mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    const float FATOR_ACEL = 9.80665 / 16384.0;
+    const float FATOR_GIRO = 1.0 / 131.0;
+
+    ax_ms2 = ax * FATOR_ACEL;
+    ay_ms2 = ay * FATOR_ACEL;
+    az_ms2 = az * FATOR_ACEL;
+
+    gx_dps = gx * FATOR_GIRO;
+    gy_dps = gy * FATOR_GIRO;
+    gz_dps = gz * FATOR_GIRO;
+
     
     Serial.print("\033[2J\033[H");
 
     Serial.print("Aceleracao (X, Y, Z): ");
-    Serial.print(ax); Serial.print(", ");
-    Serial.print(ay); Serial.print(", ");
-    Serial.print(az);
+    Serial.print(ax_ms2); Serial.print(" m/s^2, ");
+    Serial.print(ay_ms2); Serial.print(" m/s^2, ");
+    Serial.print(az_ms2); Serial.print(" m/s^2");
     Serial.print(" | Giro (X, Y, Z): ");
-    Serial.print(gx); Serial.print(", ");
-    Serial.print(gy); Serial.print(", ");
-    Serial.println(gz);
-    
+    Serial.print(gx_dps); Serial.print(" dps, ");
+    Serial.print(gy_dps); Serial.print(" dps, ");
+    Serial.print(gz_dps); Serial.println(" dps");
 
     delay(100);
 }
